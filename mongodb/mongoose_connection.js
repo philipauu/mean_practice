f76c4a273c65b03581f9527100acf8235c2db148
philipauu <Phil@Phils-MacBook.local>
2017-11-20 11:52:10 +0800

    186,000 Miles per Second.  It's not just a good idea.  IT'S THE LAW.

---
+++ mongodb/mongoose_connection.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27500/mean_users', {
+    useMongoClient: true
+});
+
+var schema = new.mongoose.Schema({
+    user: String
+});
+
+var USERCLASS = mongoose.model('users', schema);
+
+module.exports = USERCLASS;
--- routes/main_routes.js
+++ routes/main_routes.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+var USERCLASS = require('../mongodb/mongoose_connection');
 module.exports = router;
 
 router.get('/', do_homepage);
@@ -9,9 +10,62 @@ function do_homepage(req, res) {
 }
 
 // api from here down
-
 router.get('/api/read', do_read);
 router.post('/api/create', do_create);
 router.put('/api/update', do_update);
 router.delete('/api/delete/:_id', do_delete);
 
+function do_read(req, res) {
+    console.log('reading all records');
+    USERCLASS.find()
+        .then(function (results) {
+            console.log(results);
+            res.json(results);
+        });
+}
+
+function do_create(req, res) {
+    console.log('creating records');
+    console.log('req.body'); // = {user: 'bill}
+
+    var user = new USERCLASS(req.body);
+    user.save()
+        .then(function (result) {
+            console.log(result);
+            res.json({
+                message: 'saved'
+            });
+        });
+}
+
+function do_update(req, res) {
+    console.log('updating records');
+    console.log('req.body');
+
+    var update = {
+        $set: {
+            user: req.body.user
+        }
+    }
+
+    USERCLASS.findByIdAndUpdate(req.body._id, update)
+        .then(function (result) {
+            console.log(result);
+            res.json({
+                message: 'updated'
+            });
+        });
+}
+
+function do_delete(req, res) {
+    console.log('deleting records');
+    console.log('req.params');
+
+    USERCLASS.findByIdAndRemove(req.params._id)
+        .then(function (result) {
+            console.log(result);
+            res.json({
+                message: 'deleted'
+            });
+        });
+}
