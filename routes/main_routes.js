7bfff80ab5427ca29377a4d660a99e6119e1e4c3
philipauu <Phil@Phils-MacBook.local>
2017-11-20 11:09:01 +0800

    first commit for mean_practice

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -7,3 +7,11 @@ function do_homepage(req, res) {
     console.log('doing homepage');
     res.render('index');
 }
+
+// api from here down
+
+router.get('/api/read', do_read);
+router.post('/api/create', do_create);
+router.put('/api/update', do_update);
+router.delete('/api/delete/:id', do_delete);
+
